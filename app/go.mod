14f1696813112cdcf1a6c6485dc416947de912c7
Viacheslav Poturaev <vearutop@gmail.com>
2022-11-15 21:00:29 +0100

    Update deps in presence of v2.0.0 and v1.0.0

--- app/go.mod
+++ app/go.mod
@@ -3,6 +3,6 @@ module app
 go 1.19
 
 require (
-	github.com/vearutop/monorepo-playground/deep/another v0.0.0-20221115195248-9e8b3614d0ac
-	github.com/vearutop/monorepo-playground/go v0.0.0-20221115195248-9e8b3614d0ac
+	github.com/vearutop/monorepo-playground/deep/another v0.0.0-20221115195614-77518b3db090
+	github.com/vearutop/monorepo-playground/go v0.0.0-20221115195614-77518b3db090
 )
--- app/main.go
+++ app/main.go
@@ -21,5 +21,22 @@ func main() {
 // go get -u github.com/vearutop/monorepo-playground/go@v2.0.0
 // go: module github.com/vearutop/monorepo-playground@v2.0.0 found (v2.0.0+incompatible), but does not contain package github.com/vearutop/monorepo-playground/go
 
-//  go get -u github.com/vearutop/monorepo-playground/deep/another@v2.0.0
+// go get -u github.com/vearutop/monorepo-playground/deep/another@v2.0.0
 // go: module github.com/vearutop/monorepo-playground@v2.0.0 found (v2.0.0+incompatible), but does not contain package github.com/vearutop/monorepo-playground/deep/another
+
+// .........
+
+// Second tag is created as v1.0.0
+
+// go get -u github.com/vearutop/monorepo-playground/deep/another@v1.0.0
+// go: downloading github.com/vearutop/monorepo-playground v1.0.0
+// go: module github.com/vearutop/monorepo-playground@v1.0.0 found, but does not contain package github.com/vearutop/monorepo-playground/deep/another
+
+// go get -u github.com/vearutop/monorepo-playground/go@v1.0.0
+// go: module github.com/vearutop/monorepo-playground@v1.0.0 found, but does not contain package github.com/vearutop/monorepo-playground/go
+
+// go get -u
+// go: downloading github.com/vearutop/monorepo-playground/deep/another v0.0.0-20221115195614-77518b3db090
+// go: downloading github.com/vearutop/monorepo-playground/go v0.0.0-20221115195614-77518b3db090
+// go: upgraded github.com/vearutop/monorepo-playground/deep/another v0.0.0-20221115195248-9e8b3614d0ac => v0.0.0-20221115195614-77518b3db090
+// go: upgraded github.com/vearutop/monorepo-playground/go v0.0.0-20221115195248-9e8b3614d0ac => v0.0.0-20221115195614-77518b3db090
